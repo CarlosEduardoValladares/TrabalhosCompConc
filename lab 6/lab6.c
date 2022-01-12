4ef16801a82408bfe54d26fa39ce81c744b057bf
Carlos Eduardo Valladares <carloseduardovdm@gmail.com>
2022-01-11 23:50:30 -0300

    Adcionando arquivo do lab6 (Por algum motivo, só funciona para N=1 e N=2)

---
+++ lab 6/lab6.c
@@ -0,0 +1,193 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int N_threads;
+int* vetor;
+
+//variaveis do problema
+int escrito = 0;
+int somado = 0;
+int iter = 0;
+
+//variaveis para sincronizacao
+pthread_mutex_t mutex_soma, mutex_escrita, mutex_acesso, mutex_iter;
+pthread_cond_t cond_soma, cond_escrita, cond_iter;
+
+void* funcao_thread(void* arg){
+
+	int* endereco_meu_id = arg;
+	int meu_id = *endereco_meu_id;
+	int* somatorio = (int*) malloc(sizeof(int));
+	*somatorio = 0;
+	
+	for(int i = 0; i < N_threads; i++){
+	
+		printf("Thread %d: Somando todo o vetor\n", meu_id);
+		for(int j = 0; j < N_threads; j++){
+		
+			*somatorio += vetor[j];
+		}
+		
+		pthread_mutex_lock(&mutex_acesso);
+		somado++;
+		if(somado < N_threads){
+		
+			pthread_mutex_unlock(&mutex_acesso);
+			printf("Thread %d: Aguardando as outras threads terminarem seus somatórios.\n", meu_id);			
+			pthread_cond_wait(&cond_soma, &mutex_soma);
+			printf("Saí do wait soma, id: %d\n", meu_id);
+		}
+		
+		if(somado == N_threads){
+			
+			printf("Thread %d: Último somatório feito. Liberando as Threads...\n", meu_id);
+			somado = 0;
+			pthread_mutex_unlock(&mutex_acesso);
+			pthread_cond_broadcast(&cond_soma);
+			
+		}
+		
+		int aleatorio = ((int)rand())%10;
+		pthread_cond_broadcast(&cond_soma);
+		
+		printf("Thread %d: Liberada da trava de soma! Escrevendo novo valor (%d) em Vetor[%d]\n",
+				meu_id,
+				aleatorio, 
+				meu_id-1);
+				
+		vetor[meu_id-1] = aleatorio;
+		pthread_mutex_lock(&mutex_acesso);
+		escrito++;
+		
+		if(escrito < N_threads){
+		
+			printf("Thread %d: Aguardando as outras threads terminarem de escrever.\n", meu_id);
+			pthread_mutex_unlock(&mutex_acesso);
+			pthread_cond_wait(&cond_escrita, &mutex_escrita);
+		}
+		
+		if(escrito == N_threads){
+			
+			printf("Thread %d: Última escrita feita. Liberando as Threads...\n", meu_id);
+			escrito = 0;
+			pthread_mutex_unlock(&mutex_acesso);
+			pthread_cond_broadcast(&cond_escrita);
+			
+		}
+		
+		printf("Thread %d: Liberada da trava de escrita!.\n", meu_id);
+		printf("Somatório parcial: %d\n", *somatorio);
+		
+		
+	}
+	
+	printf("Thread %d: Meu somatório final: %d\n", meu_id, *somatorio);
+	pthread_exit( (void*) somatorio);
+
+	pthread_mutex_lock(&mutex_acesso);
+	iter++;
+	
+	if(iter < N_threads){
+	
+		printf("Aguardando fim de iteração.\n");
+		pthread_mutex_unlock(&mutex_acesso);
+		pthread_cond_wait(&cond_iter, &mutex_iter);	
+	}
+	
+	if(iter == N_threads){
+	
+		printf("Ultima iteração, liberando...\n");
+		pthread_mutex_unlock(&mutex_acesso);
+		pthread_cond_broadcast(&cond_iter);	
+	}
+	
+}
+
+//funcao principal
+int main(int argc, char* argv[]) {
+
+  if(argc < 2){
+  	printf("Chame o programa passando o Número de threads como argumento.\n");
+  	printf("Ex: ./lab6.out 3");
+  }
+  
+  N_threads = atoi(argv[1]);
+  vetor = (int*) malloc(sizeof(int)*N_threads);
+  int somatorio_final[N_threads];
+  void* retorno = NULL;
+
+  srand(time(0));
+  
+  puts("");
+  printf("Disposição inicial do vetor:\n");
+  for(int i = 0; i < N_threads; i++){
+  
+  	vetor[i] = ((int)rand())%10;
+  	printf("Vetor[%d]: %d\n", i, vetor[i]);
+  	
+  }
+  puts("");
+
+  //identificadores das threads
+  pthread_t tid[N_threads];
+  int id[N_threads];
+
+  //inicializa as variaveis de sincronizacao
+  pthread_mutex_init(&mutex_soma, NULL);
+  pthread_mutex_init(&mutex_escrita, NULL);
+  pthread_mutex_init(&mutex_acesso, NULL);
+  pthread_cond_init(&cond_soma, NULL);
+  pthread_cond_init(&cond_escrita, NULL);
+
+  //cria as threads
+  for(int i=0; i<N_threads; i++) {
+  
+    id[i] = i+1;
+    if(pthread_create(&tid[i], NULL, funcao_thread, (void *) &id[i])) exit(-1);
+  } 
+
+  for(int i=0; i<N_threads; i++) {
+  
+		pthread_join(*(tid+i), &retorno);
+		somatorio_final[i] = *((int*) retorno);
+  }
+  
+  puts("");
+  printf("Disposição final do vetor:\n");
+  for(int i = 0; i < N_threads; i++){
+  
+  	printf("Vetor[%d]: %d\n", i, vetor[i]);
+  	
+  }
+  puts("");
+  
+  printf("Valores finais retornados das threads:\n");
+  for(int i = 0; i < N_threads; i++){
+  
+  	printf("Somatório da Thread %d: %d\n", i+1, somatorio_final[i]);
+  	
+  }
+  puts("");
+  
+  int igual = 0;
+  for(int i = 0; i < N_threads-1; i++){
+  
+  	if(somatorio_final[i] != somatorio_final[i+1]){
+  		igual = 1;
+  	}
+  	
+  }
+  
+  if(igual == 0){
+  
+  	printf("Todos os resultados retornados das threads são iguais\n")  ;
+  } else {
+  
+  	printf("Algum resultado das threads difere\n");  
+  }
+  puts("");
+   
+  return 0;
+}
